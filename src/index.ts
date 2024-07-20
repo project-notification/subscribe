edeb11776c83919cb7162b1e4ca05d3a255dfb90
leemhoon00 <leemhoon000@gmail.com>
2024-07-20 18:01:23 +0900

    ci build script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import { Handler, APIGatewayEvent } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
 
 export const handler: Handler = async (event: APIGatewayEvent) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello World!',
-    }),
-  };
+  const data: {
+    email: string;
+    topics?: string[];
+  } = JSON.parse(event.body!);
+
+  const client = new DynamoDBClient({});
+
+  const command = new PutCommand({
+    TableName: 'subscription',
+    Item: {
+      ...data,
+    },
+  });
+
+  const result = await client.send(command);
+  if (result.$metadata.httpStatusCode === 200) {
+    return {
+      statusCode: 201,
+    };
+  } else {
+    console.error('Fail');
+    console.error('data: ', data);
+    console.error('result: ', result);
+    return {
+      statusCode: 500,
+    };
+  }
 };
